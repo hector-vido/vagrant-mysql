4fc1494ab94f73899dcfdfdeaf6aa4a7d3caa359
Hector Vido <hector_vido@yahoo.com.br>
2020-07-13 15:55:14 -0300

    Exercicio dump/restore

---
+++ exercicios/aula-03/dump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# SQL
+
+mysqldump employees --compact --tables employees titles | pigz -9 > /root/employees-titles.sql.gz
+
+# CSV
+
+mkdir /var/lib/mysql-files/employees
+chown mysql: /var/lib/mysql-files/employees
+mysqldump employees --tables titles employees --tab=/var/lib/mysql-files/employees
+
+# SQL
+
+mysql -e "CREATE DATABASE employees_sql"
+time zcat employees-titles.sql.gz | mysql employees_sql
+
+
+# CSV
+
+mysql -e "CREATE DATABASE employees_csv"
+cd /var/lib/mysql-file/employees
+for X in *.sql; do mysql -f employees_csv < $X; done
+mysql -e 'SET GLOBAL foreign_key_checks=0'
+mysqlimport --use-threads=4 employees_csv $PWD/*.txt
+mysql -e 'SET GLOBAL foreign_key_checks=1'
