fa8eb384c9929722c4c11ece7d4d0ff682cd975d
Hector Vido <hector_vido@yahoo.com.br>
2020-07-19 13:37:22 -0300

    Criado esquema do banco

--- files/4linux/banco.sql
+++ files/4linux/banco.sql
@@ -13,7 +13,7 @@ CREATE TABLE cursos (
   alterado DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO cursos (id, nome, duracao, valor) VALUES (450, 'Linux Beginners', 0, 0),(451, 'Linux Administrator', 0, 0),(452, 'Linux Engineer', 0, 0),(801, 'MySQL', 0, 0),(802, 'PostgreSQL', 0, 0),(803, 'MongoDB', 0, 0),(500, 'PHP Básico', 0, 0),(501, 'PHP Intermediário', 0, 0),(502, 'PHP Avançado', 0, 0),(520, 'Python Básico', 0, 0),(521, 'Python para Administradores', 0, 0),(522, 'Python para Cientistas de Dados', 0, 0),(525, 'Infraestrutura Ágil', 0, 0),(540, 'Docker', 0, 0),(541, 'Kubernetes', 0, 0),(542, 'Openshift', 0, 0),(543, 'Rancher', 0, 0),(600, 'pfSense', 0, 0),(700, 'OpenLDAP e Samba', 0, 0),(900, 'Segurança em Servidores Linux', 0, 0);
+INSERT INTO cursos (id, nome, duracao, valor) VALUES (450, 'Linux Beginners', 40, 1000.00),(451, 'Linux Administrator', 40, 1200.00),(452, 'Linux Engineer', 40, 1500.00),(801, 'MySQL', 40, 1400.00),(802, 'PostgreSQL', 40, 1400.00),(803, 'MongoDB', 40, 1400.00),(500, 'PHP Básico', 40, 1200.00),(501, 'PHP Intermediário', 40, 1200.00),(502, 'PHP Avançado', 40, 1200.00),(520, 'Python Básico', 40, 1200.00),(521, 'Python para Administradores', 40, 1400.00),(522, 'Python para Cientistas de Dados', 40, 1500.00),(525, 'Infraestrutura Ágil', 40, 1600.00),(540, 'Docker', 40, 1500.00),(541, 'Kubernetes', 40, 1500.00),(542, 'Openshift', 20, 1000.00),(543, 'Rancher', 20, 1000.00),(600, 'pfSense', 20, 800.00),(700, 'OpenLDAP e Samba', 20, 800.00),(900, 'Segurança em Servidores Linux', 40, 1500.00);
 
 CREATE TABLE professores (
   cpf CHAR(14) PRIMARY KEY,
@@ -36,9 +36,9 @@ CREATE TABLE alunos (
 
 CREATE TABLE turmas (
   id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL DEFAULT ''
+  nome VARCHAR(100) NOT NULL DEFAULT '',
   inicio DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00',
-  fim DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00',
+  fim DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00'
 );
 
 CREATE TABLE turmas_alunos (
