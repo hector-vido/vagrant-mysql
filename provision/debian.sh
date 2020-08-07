41d55c6a8f508eba50bb7d561a99f0a3cfb61886
Hector Vido <hector_vido@yahoo.com.br>
2020-08-07 13:43:57 -0300

    Adicionado instalação do MariaDB

--- provision/debian.sh
+++ provision/debian.sh
@@ -11,6 +11,9 @@ chmod 400 /root/.ssh/*
 rm -rf /etc/apt/sources.list.d/mysql.list
 apt-get update && apt-get install -y gnupg vim
 
+#apt-get install -y mariadb-server
+#exit
+
 # Adiciona chave e repositório
 wget -q -O - https://dev.mysql.com/doc/refman/8.0/en/checking-gpg-signature.html | grep -zEo -- '-----BEGIN.*BLOCK-----' | apt-key add -
 cat > /etc/apt/sources.list.d/mysql.list <<EOF
