e37182bb897ebb1c4dbb44801f6dbb9d9e06a28c
Hector Vido <hector_vido@yahoo.com.br>
2021-05-04 16:03:50 -0300

    Adicionado 3 forks

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,21 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
+#
+# forks: mysql, mariadb ou percona
 
 vms = {
-  'db1' => {'memory' => '1024', 'cpus' => 2, 'ip' => '10', 'box' => 'debian/buster64', 'provision' => 'debian.sh'},
-  'db2' => {'memory' => '1024', 'cpus' => 2, 'ip' => '20', 'box' => 'debian/buster64', 'provision' => 'debian.sh'},
-  'db3' => {'memory' => '1024', 'cpus' => 2, 'ip' => '30', 'box' => 'debian/buster64', 'provision' => 'debian.sh'}
-  #'haproxy' => {'memory' => '256', 'cpus' => 1, 'ip' => '40', 'box' => 'debian/buster64', 'provision' => 'haproxy.sh'}
-  #'centos' => {'memory' => '1024', 'cpus' => 2, 'ip' => '40', 'box' => 'centos/8', 'provision' => 'centos.sh'},
-  #'opensuse' => {'memory' => '1024', 'cpus' => 2, 'ip' => '50', 'box' => 'opensuse/Tumbleweed.x86_64', 'provision' => 'opensuse.sh'}
+  'db1' => {'memory' => '1024', 'cpus' => 2, 'ip' => '10', 'box' => 'debian', 'script' => 'debian.sh', 'fork' => 'mysql', 'sample' => 1},
+  'db2' => {'memory' => '1024', 'cpus' => 2, 'ip' => '20', 'box' => 'debian', 'script' => 'debian.sh', 'fork' => 'mysql', 'sample' => 0},
+  'db3' => {'memory' => '1024', 'cpus' => 2, 'ip' => '30', 'box' => 'debian', 'script' => 'debian.sh', 'fork' => 'mysql', 'sample' => 0},
+  #'centos' => {'memory' => '1024', 'cpus' => 2, 'ip' => '40', 'box' => 'centos', 'script' => 'centos.sh', 'fork' => 'mysql', 'sample' => 1},
+  #'opensuse' => {'memory' => '1024', 'cpus' => 2, 'ip' => '50', 'box' => 'opensuse', 'script' => 'opensuse.sh', 'fork' => 'mysql', 'sample' => 1},
+  #'haproxy' => {'memory' => '256', 'cpus' => 1, 'ip' => '40', 'box' => 'debian', 'script' => 'haproxy.sh'},
+}
+
+boxes = {
+  'debian'   => 'debian/buster64',
+  'centos'   => 'centos/8',
+  'opensuse' => 'opensuse/Tumbleweed.x86_64'
 }
 
 Vagrant.configure('2') do |config|
@@ -16,10 +24,10 @@ Vagrant.configure('2') do |config|
 
   vms.each do |name, conf|
     config.vm.define "#{name}" do |my|
-      my.vm.box = conf['box']
+      my.vm.box = boxes[conf['box']]
       my.vm.hostname = "#{name}.example.com"
       my.vm.network 'private_network', ip: "172.27.11.#{conf['ip']}"
-      my.vm.provision 'shell', path: "provision/#{conf['provision']}"
+      my.vm.provision 'shell', path: "provision/#{conf['script']}", args: [conf['fork'], conf['sample']]
       my.vm.provider 'virtualbox' do |vb|
         vb.memory = conf['memory']
         vb.cpus = conf['cpus']
--- provision/debian.sh
+++ provision/debian.sh
@@ -15,49 +15,48 @@ if [ "$(swapon -v)" == "" ]; then
   echo '/swapfile       swap    swap    defaults        0       0' >> /etc/fstab
 fi
 
-# Atualiza pacotes e prepara a máquina
-rm -rf /etc/apt/sources.list.d/mysql.list
+apt-get update && apt-get install -y gnupg2 vim
 
-exit
+if [ "$1" == 'mariadb' ]; then # MariaDB
+	apt-get install -y software-properties-common dirmngr apt-transport-https
+	echo 'deb [arch=amd64] https://mirror1.cl.netactuate.com/mariadb/repo/10.5/debian buster main' > /etc/apt/sources.list.d/mariadb.list
+	apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+	apt-get update
+	apt-get install -y mariadb-server
+	systemctl start mariadb
+	systemctl enable mariadb
+elif [ "$1" == 'percona' ]; then # Percona
+	wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+	dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+	percona-release setup ps80
+	debconf-set-selections <<< 'percona-server-server percona-server-server/root-pass password 4linux'
+	debconf-set-selections <<< 'percona-server-server percona-server-server/re-root-pass password 4linux'
+	DEBIAN_FRONTEND=noninteractive apt-get install -y percona-server-server
+else # MySQL
+	apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
+	echo 'deb http://repo.mysql.com/apt/debian/ buster mysql-8.0 mysql-tools' > /etc/apt/sources.list.d/mysql.list
 
-apt-get update && apt-get install -y gnupg vim
-
-#apt-get install -y mariadb-server
-#exit
-
-# Adiciona chave e repositório
-wget -q -O - https://dev.mysql.com/doc/refman/8.0/en/checking-gpg-signature.html | grep -zEo -- '-----BEGIN.*BLOCK-----' | apt-key add -
-cat > /etc/apt/sources.list.d/mysql.list <<EOF
-deb http://repo.mysql.com/apt/debian/ buster mysql-8.0
-deb http://repo.mysql.com/apt/debian/ buster mysql-tools
-EOF
-apt-get update
-
-# Ajusta para instalação não assistida
-debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password 4linux'
-debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password 4linux'
-DEBIAN_FRONTEND='noninteractive' apt-get install -y mysql-community-server mysql-shell mysql-router
-
-# Configura o client
-#echo -e '[client]\nuser=root\npassword=4linux' > /root/.my.cnf
-mysql -u root -p4linux -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '4linux'" > /dev/null
-echo "export PROMPT_COMMAND='history -a'" > /root/.bashrc
-
-if [ "$(grep report_host /etc/mysql/mysql.conf.d/mysqld.cnf)" == "" ]; then
-  echo "report_host = $(ip a | grep -Eo '172.27.11.[0-9]{2}' | grep -v 25)" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+	apt-get update
+	
+	debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password 4linux'
+	debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password 4linux'
+	DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-community-server mysql-shell mysql-router
+	
+	grep report_host /etc/mysql/mysql.conf.d/mysqld.cnf > /dev/null
+	if [ "$?" -ne 0 ]; then
+		IP=$(ip a | grep -Eo '172.27.11.[0-9]{2}' | grep -v 25)
+		echo "report_host = $IP" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+	fi
+	systemctl restart mysql
 fi
-systemctl restart mysql
 
-if [ "$HOSTNAME" == "db1" ]; then
+if [ "$2" -eq 1 ]; then
 	apt-get install -y git
-	cd
-	git clone https://github.com/datacharmer/test_db.git
-	wget -q https://downloads.mysql.com/docs/sakila-db.tar.gz
-	cd ~/test_db
+	git clone --depth 1 https://github.com/datacharmer/test_db.git ~/employees-db
+	wget -q https://downloads.mysql.com/docs/sakila-db.tar.gz -O - | tar -xzv -C ~/
+	cd ~/employees-db
 	mysql -u root -p4linux < employees.sql
-	cd
-	tar -xzf sakila-db.tar.gz
-	cd ~/sakila-db/
+	cd ~/sakila-db
 	mysql -u root -p4linux < sakila-schema.sql
 	mysql -u root -p4linux < sakila-data.sql
 fi
